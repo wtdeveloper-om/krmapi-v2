58d4cf2a8372f52526f7ec20ace96a44f312b589
Om Dev WT <wtdeveloper.om@gmail.com>
2026-01-09 14:50:26 +0530

    modified firebase cred & handling

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,15 +1,16 @@
 import admin from "firebase-admin";
 
-const serviceAccount = {
-  project_id: process.env.FIREBASE_PROJECT_ID!,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL!,
-  private_key: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-};
+const serviceAccount = JSON.parse(
+  Buffer.from(
+    process.env.FIREBASE_SERVICE_ACCOUNT_BASE64!,
+    "base64"
+  ).toString("utf8")
+);
 
-admin.initializeApp({
-  credential: admin.credential.cert(
-    serviceAccount as admin.ServiceAccount
-  ),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export default admin;
